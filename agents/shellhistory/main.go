cb0b6e82fa9df0bee002dae9ab7065f598ff0601
tarball0 <chrispaul.tar.gz@proton.me>
2025-04-24 01:46:04 +0530

    made shell history recognize various shells

--- agents/shellhistory/main.go
+++ agents/shellhistory/main.go
@@ -11,21 +11,31 @@ func Run() {
 	// Set default limit to 10
 	n := flag.Int("n", 10, "Number of history items to fetch (default: 10)")
 	flag.Parse()
+	shellpath := os.Getenv("SHELL")
+	shellarr := strings.Split(string(shellpath), "/")
+	shell := shellarr[len(shellarr)-1]
+	var HistoryFile string
 
-	zshHistoryFile := os.Getenv("HOME") + "/.zsh_history"
+	if shell == "zsh" {
+		HistoryFile = os.Getenv("HOME") + "/.zsh_history"
+	} else if shell == "bash" {
+		HistoryFile = os.Getenv("HOME") + "/.bash_history"
+	} else if shell == "fish" {
+		HistoryFile = os.Getenv("HOME") + "/.local/share/fish/fish_history"
+	}
 
 	// If n is <= 0, default to 10
 	if *n <= 0 {
 		*n = 10
 	}
 
-	printLimitedHistory(zshHistoryFile, *n)
+	printLimitedHistory(HistoryFile, *n, shell)
 }
 
-func printLimitedHistory(filename string, n int) {
+func printLimitedHistory(filename string, n int, shell string) {
 	content, err := os.ReadFile(filename)
 	if err != nil {
-		fmt.Println("Error reading .zsh_history file:", err)
+		fmt.Printf("Error reading %s shell history file:\n%s\n", shell, err)
 		return
 	}
 	lines := strings.Split(string(content), "\n")
@@ -33,7 +43,7 @@ func printLimitedHistory(filename string, n int) {
 	if start < 0 {
 		start = 0
 	}
-	fmt.Printf("Last %d lines of .zsh_history file:\n", n)
+	fmt.Printf("Last %d lines of %s history file:\n", n, shell)
 	for _, line := range lines[start:] {
 		fmt.Println(line)
 	}
